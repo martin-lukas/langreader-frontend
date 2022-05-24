96a58e5f8b0e209811fdb0f104ae2f489b85b65e
martin-lukas <martinlukas24@protonmail.com>
2022-05-24 15:19:49 +0200

    Set up dev/prod configuration and scripts

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,12 +1,12 @@
 import Axios from "axios";
 
-const axiosInstance = Axios.create({baseURL: "http://localhost:8080/api"});
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const axiosInstance = Axios.create({baseURL: `${isDevelopment ? "http://localhost:8080" : ""}/api`});
 
 axiosInstance.interceptors.request.use((config) => {
     config.headers["Content-type"] = "application/json";
     return config;
 });
-
-
 
 export default axiosInstance;
