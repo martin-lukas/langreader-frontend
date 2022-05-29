5afe19eaadd5dc49e7c04ade732a562cd5effdff
martin-lukas <martinlukas24@protonmail.com>
2022-05-29 20:12:06 +0200

    Simplify signup page state and error handling

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -6,47 +6,82 @@ import {useAppContext} from "../../context/AppContext";
 import {useLoader} from "../common/LoaderHook";
 import Loader from "../common/Loader";
 
+interface SignupFields {
+    username: string;
+    password: string;
+    confirmPassword: string;
+    nativeLang?: Language;
+}
+
+const FIELDS_DEFAULT: SignupFields = {
+    username: "",
+    password: "",
+    confirmPassword: "",
+};
+
 const Signup: React.FC = () => {
     const {isLoading, startLoading, stopLoading} = useLoader(false);
     const context = useAppContext();
     const allLanguages = context.allLanguages as Language[];
 
-    const [username, setUsername] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const [confirmPassword, setConfirmPassword] = useState<string>("");
-    const [nativeLanguage, setNativeLanguage] = useState<Language>();
+    const [fields, setFields] = useState<SignupFields>(FIELDS_DEFAULT);
 
     const [infoMessage, setInfoMessage] = useState<string>("");
     const [errorMessage, setErrorMessage] = useState<string>("");
 
-    const handleNativeLanguageChange = (event: any) => {
-        setNativeLanguage(allLanguages[event.target.value]);
+    const handleChange = (name: string, value: string | Language): void => {
+        setFields({
+            ...fields,
+            [name]: value
+        });
     };
 
-    const handleSubmit = async (event: any) => {
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        handleChange(event.target.name, event.target.value);
+    };
+
+    const handleNativeLangChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+        handleChange(event.target.name, allLanguages[Number(event.target.value)]);
+    };
+
+    const trimFields = (fields: SignupFields): SignupFields => {
+        return {
+            ...fields,
+            username: fields.username.trim(),
+            password: fields.password.trim(),
+            confirmPassword: fields.confirmPassword.trim(),
+        };
+    };
+
+    const validate = (formData: SignupFields) => {
+        let errorMsg = "";
+        if (!formData.username || !formData.password || !formData.confirmPassword || !formData.nativeLang) {
+            errorMsg = "Make sure you fill out all the fields.";
+        } else if (formData.password !== formData.confirmPassword) {
+            errorMsg = "Make sure the passwords match.";
+        }
+        setErrorMessage(errorMsg);
+        return !errorMsg;
+    };
+
+    const handleSubmit = async (event: any): Promise<void> => {
         event.preventDefault();
+
         setInfoMessage("");
-        setErrorMessage("");
-        if (username && password && confirmPassword && nativeLanguage) {
-            if (password === confirmPassword) {
-                startLoading();
-                signupUser(username, password, nativeLanguage)
-                    .then(() => {
-                        setInfoMessage("Registration successful! You can log in now.");
-                        setUsername("");
-                        setPassword("");
-                        setConfirmPassword("");
-                        setNativeLanguage(undefined);
-                    })
-                    .catch(() => {
-                        setErrorMessage("The registration wasn't successfull. Please try again later.");
-                    })
-                    .finally(stopLoading);
-            } else {
-                setErrorMessage("Make sure the passwords match.");
-            }
-        } else {
-            setErrorMessage("Make sure you fill out all the fields.");
+
+        const trimmedFields = trimFields(fields);
+
+        if (validate(trimmedFields)) {
+            startLoading();
+            signupUser(trimmedFields.username, trimmedFields.password, trimmedFields.nativeLang!)
+                .then(() => {
+                    setInfoMessage("Registration successful! You can log in now.");
+                    setFields(FIELDS_DEFAULT);
+                })
+                .catch(() => {
+                    setErrorMessage("The registration wasn't successfull. Please try again later.");
+                })
+                .finally(stopLoading);
         }
     };
 
@@ -69,8 +104,8 @@ const Signup: React.FC = () => {
                             id="username-input"
                             name="username"
                             type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
+                            value={fields.username}
+                            onChange={handleInputChange}
                         />
                     </div>
                     <div className="lower-form-group">
@@ -79,25 +114,25 @@ const Signup: React.FC = () => {
                             id="password-input"
                             name="password"
                             type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            value={fields.password}
+                            onChange={handleInputChange}
                         />
                     </div>
                     <div className="lower-form-group">
                         <label htmlFor="confirm-password-input">Confirm password</label>
                         <input
                             id="confirm-password-input"
-                            name="confirm-password"
+                            name="confirmPassword"
                             type="password"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            value={fields.confirmPassword}
+                            onChange={handleInputChange}
                         />
                     </div>
                     <div className="lower-form-group">
                         <label id="lang-label" htmlFor="native-lang-select">
                             Select Your Native Language:
                         </label>
-                        <select onChange={handleNativeLanguageChange} id="native-lang-select">
+                        <select onChange={handleNativeLangChange} id="native-lang-select" name="nativeLang">
                             <option value={undefined} defaultValue={undefined}>Choose Language</option>
                             {allLanguages.map((language, index) => (
                                 <option value={index} key={language.id}>
@@ -107,7 +142,7 @@ const Signup: React.FC = () => {
                         </select>
                     </div>
                     <div className="auth-submit-button-div lower-form-group">
-                        <button className="auth-submit-button">Login</button>
+                        <button className="auth-submit-button">Sign Up</button>
                     </div>
                 </form>
             </div>
